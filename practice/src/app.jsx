02cc6eac4061048632408068530baeaf24616940
singhrajesh8699 <singhrajesh8699@gmail.com>
2017-06-20 12:32:02 +0530

    react

---
+++ practice/src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div className="col">
+
+            Hello World!!!
+         </div>
+      );
+   }
+}
+
+export default App;
---
+++ practice/webpack.config.js
@@ -0,0 +1,41 @@
+
+var path = require('path');
+var webpack=require('webpack');
+
+var app=path.resolve(__dirname,'src');
+
+var config = {
+
+   entry: app+'/main.js',
+	
+   output: {
+      path: __dirname+'/src',
+      filename: 'bundle.js',
+   },
+	
+   devServer: {
+      inline: true,
+
+      port: 8080
+   },
+	
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+				
+            query: {
+               presets: ['es2015', 'react']
+            }
+         },
+         {
+         	test:/(\.scss|\.css)$/,
+         	loader:'style!css'
+         }
+      ]
+   }
+}
+
+module.exports = config;
